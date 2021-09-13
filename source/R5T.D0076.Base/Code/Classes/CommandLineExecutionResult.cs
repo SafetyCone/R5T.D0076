c3537754108edc83e8062c45bf6290ca1662868e
David Coats <david@getsafetycone.com>
2021-09-13 14:13:26 -0700

    Additions to ICommandLineOperator functionality.

---
+++ source/R5T.D0076.Base/Code/Classes/CommandLineExecutionResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.D0076
+{
+    public class CommandLineExecutionResult
+    {
+        public int ExitCode { get; set; }
+        public string[] OutputLines { get; set; }
+        public string[] ErrorLines { get; set; }
+    }
+}
--- source/R5T.D0076.Base/Code/Services/Extensions/ICommandLineOperatorExtensions.cs
+++ source/R5T.D0076.Base/Code/Services/Extensions/ICommandLineOperatorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using R5T.D0076;
@@ -16,6 +17,38 @@ namespace System
             }
 
             return commandLineOperator.Run(command, arguments, WriteToConsole, WriteToConsole);
+        }
+
+        public static async Task<CommandLineExecutionResult> RunGetResult(this ICommandLineOperator commandLineOperator, string command, string arguments = Strings.Empty)
+        {
+            var outputLines = new List<string>();
+
+            Task WriteToOutput(string line)
+            {
+                outputLines.Add(line);
+
+                return Task.CompletedTask;
+            }
+
+            var errorLines = new List<string>();
+
+            Task WriteToError(string line)
+            {
+                errorLines.Add(line);
+
+                return Task.CompletedTask;
+            }
+
+            var exitCode = await commandLineOperator.Run(command, arguments, WriteToOutput, WriteToError);
+
+            var output = new CommandLineExecutionResult
+            {
+                ErrorLines = errorLines.ToArray(),
+                ExitCode = exitCode,
+                OutputLines = outputLines.ToArray(),
+            };
+
+            return output;
         }
     }
 }
